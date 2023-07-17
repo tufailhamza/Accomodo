4c3290b90c265850ccc53d264d0e7a096a254c9e
Muhammad Hamza <Hamza.tufail504@gmail.com>
2023-07-18 01:11:07 +0500

    Add start scritp

--- app.js
+++ app.js
@@ -175,7 +175,8 @@ app.use((err,req,res,next)=>
   const {status=500 , message = 'Something went Wrong'}=err;
   res.status(status).render('error',{err})
 })
-app.listen(3000,()=>
+const port = process.env.PORT || 3000
+app.listen(port,()=>
 {
-    console.log('Serving on port 3000');
+    console.log(`Serving on port ${port}`);
 })
